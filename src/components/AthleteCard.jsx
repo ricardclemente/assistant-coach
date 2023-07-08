b3ef860c8fa0a34200f165a6e03940ba25194aad
LAPTOP-VT1PIINV\ricar <ricard.clemente@gmail.com>
2023-07-08 14:43:39 +0200

    changed athlete list improve visualization

--- src/components/AthleteCard.jsx
+++ src/components/AthleteCard.jsx
@@ -1,14 +1,35 @@
 import { useState } from "react"
+import './AthleteCard.css'
+import defaultImage from '../images/DefaultUserPicture.png';
 
 export function AthleteCard({id,name,speciality, nationality}){
     const [showNationality, setShowNationality] = useState(false)
     const handleClick=()=>{setShowNationality(!showNationality)}
+    const caretDown = <i className="fa fa-caret-down"></i>
+    const caretUp = <i className="fa fa-caret-up"></i>
     return(
         <>
-        <li key={id}> {name} - {speciality}
-        {showNationality && <div>{nationality}</div>}
+        <li key={id} className="athleteItem"> 
+            <div className="athleteCard">
+                <div className="imageBox">
+                    <img src={defaultImage} className="userimage"/>
+                    <div className="innerIcon"></div>
+                </div>
+                <div className="sideBox">
+                    <div className="spanStyle">
+                        <b>{name} </b>
+                    </div>
+                    <div className='spanStyle2'>
+                        <b>{speciality}</b> 
+                    </div>
+                    {showNationality && <span className='spanStyle2'>{nationality}</span>}
+                    <div className='spanStyle2'>
+                    <button className='athleteButton' onClick={handleClick}>{showNationality ? caretUp : caretDown}</button>
+                    </div>
+                </div>
+            </div>
         </li>
-        <button onClick={handleClick}>{showNationality ? "Hide Details" : "Show Details"}</button>
+        
         
         </> 
     )
